0facbc17a66154add21de1919b3a0496e64a8531
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2024-12-18 12:45:55 +0100

    minor changes to api

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -3,9 +3,16 @@
 diesel::table! {
     inventory (uuid) {
         uuid -> Text,
-        owneruuid -> Text,
+        owner -> Text,
         money -> Integer,
-        name -> Text,
+    }
+}
+
+diesel::table! {
+    inventoryItems (id) {
+        id -> Integer,
+        itemuuid -> Text,
+        inventoryuuid -> Text,
     }
 }
 
@@ -32,7 +39,6 @@ diesel::table! {
         presetReference -> Text,
         amount -> Integer,
         dmNote -> Text,
-        inventoryuuid -> Text,
     }
 }
 
@@ -51,20 +57,19 @@ diesel::table! {
     user (uuid) {
         uuid -> Text,
         name -> Text,
-        dm -> Integer,
     }
 }
 
-diesel::joinable!(inventory -> user (owneruuid));
+diesel::joinable!(inventoryItems -> inventory (inventoryuuid));
+diesel::joinable!(inventoryItems -> item (itemuuid));
 diesel::joinable!(inventoryReaders -> inventory (inventoryuuid));
 diesel::joinable!(inventoryReaders -> user (useruuid));
 diesel::joinable!(inventoryWriters -> inventory (inventoryuuid));
 diesel::joinable!(inventoryWriters -> user (useruuid));
-diesel::joinable!(item -> inventory (inventoryuuid));
-diesel::joinable!(item -> itempreset (presetReference));
 
 diesel::allow_tables_to_appear_in_same_query!(
     inventory,
+    inventoryItems,
     inventoryReaders,
     inventoryWriters,
     item,
