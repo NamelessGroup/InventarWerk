43a29067e5353ad6c5e305b89eeb17167d3c46d9
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2024-12-25 22:06:44 +0100

    fixed sql

--- backend/migrations/2024-12-17-120244_initial_setup/down.sql
+++ backend/migrations/2024-12-17-120244_initial_setup/down.sql
@@ -1,8 +1,8 @@
 -- This file should undo anything in `up.sql`
 
-DROP TABLE inventory;
 DROP TABLE inventory_reader;
 DROP TABLE inventory_writer;
-DROP TABLE item_preset;
-DROP TABLE user;
 DROP TABLE inventory_item;
+DROP TABLE item_preset;
+DROP TABLE inventory;
+DROP TABLE user;
--- backend/migrations/2024-12-17-120244_initial_setup/up.sql
+++ backend/migrations/2024-12-17-120244_initial_setup/up.sql
@@ -1,4 +1,19 @@
 -- Your SQL goes here
+CREATE TABLE user (
+    uuid TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    dm INTEGER NOT NULL
+);
+
+CREATE TABLE item_preset (
+    uuid TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    description TEXT NOT NULL,
+    creator TEXT NOT NULL,
+    item_type TEXT NOT Null
+);
+
 CREATE TABLE inventory (
     uuid TEXT NOT NULL PRIMARY KEY,
     owner_uuid TEXT NOT NULL,
@@ -24,21 +39,6 @@ CREATE TABLE inventory_writer (
 );
 
 
-CREATE TABLE item_preset (
-    uuid TEXT NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
-    price INTEGER NOT NULL,
-    description TEXT NOT NULL,
-    creator TEXT NOT NULL,
-    item_type TEXT NOT Null
-);
-
-CREATE TABLE user (
-    uuid TEXT NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL,
-    dm INTEGER NOT NULL
-);
-
 CREATE TABLE inventory_item(
     inventory_uuid TEXT NOT NULL,
     item_preset_uuid TEXT NOT NULL,
@@ -47,4 +47,4 @@ CREATE TABLE inventory_item(
     PRIMARY KEY(inventory_uuid, item_preset_uuid),
     FOREIGN KEY(inventory_uuid) REFERENCES inventory(uuid) ON DELETE CASCADE,
     FOREIGN KEY(item_preset_uuid) REFERENCES item_preset(uuid) ON DELETE CASCADE
-)
+);
