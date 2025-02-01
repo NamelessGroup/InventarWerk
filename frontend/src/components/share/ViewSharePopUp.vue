c8a05d1a44037302705a4a3c92308e91c871597e
Alexander Vogt <alexander_vogt@outlook.de>
2025-02-01 18:11:04 +0100

    fix format

--- frontend/src/components/share/ViewSharePopUp.vue
+++ frontend/src/components/share/ViewSharePopUp.vue
@@ -51,7 +51,6 @@ async function leaveInventory() {
     await store().removeWriteShare(props.inventory.uuid, store().uuid)
   }
   store().removeReadShare(props.inventory.uuid, store().uuid)
-  
 
   store().inventoryUuids = store().inventoryUuids.filter((uuid) => uuid !== props.inventory.uuid)
   delete store().inventories[props.inventory.uuid]
