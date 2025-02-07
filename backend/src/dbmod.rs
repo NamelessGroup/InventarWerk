c2f52a8776516e2859b6de6ca1087107d85f38ea
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2025-02-08 00:05:40 +0100

    added connection customizer

--- backend/src/dbmod.rs
+++ backend/src/dbmod.rs
@@ -2,11 +2,25 @@
 use diesel::r2d2::{self, ConnectionManager};
 use diesel::sqlite::SqliteConnection;
 use std::env;
+use diesel::RunQueryDsl;
+
+
+#[derive(Debug)]
+pub struct SqliteConnectionCustomizer;
+
+impl r2d2::CustomizeConnection<SqliteConnection, diesel::r2d2::Error> for SqliteConnectionCustomizer {
+    fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), diesel::r2d2::Error> {
+        diesel::sql_query("PRAGMA foreign_keys = ON;")
+        .execute(conn)
+        .expect("Failed to enable foreign key constraints");
+        Ok(())
+    }
+}
 
 pub type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
 pub fn establish_connection() -> DbPool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-    r2d2::Pool::builder().build(manager).expect("Failed to create pool.")
+    r2d2::Pool::builder().connection_customizer(Box::new(SqliteConnectionCustomizer)).build(manager).expect("Failed to create pool.")
 }
