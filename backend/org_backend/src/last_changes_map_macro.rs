5cc43882602966d599dc07edbbee5693fd783f8a
ProgramPhoenix <93595010+ProgramPhoenix@users.noreply.github.com>
2025-04-26 23:23:25 +0200

    added doc for macros

--- backend/org_backend/src/last_changes_map_macro.rs
+++ backend/org_backend/src/last_changes_map_macro.rs
@@ -3,10 +3,22 @@ use std::sync::Mutex;
 
 use lazy_static::lazy_static;
 
+//! # Last Changes Map Macro
+//!
+//! This module provides global state and macros for tracking and retrieving the last change timestamps of inventories.
+//! It uses a global, thread-safe `HashMap` to store the last modification time (as a `u128` UNIX timestamp in milliseconds) for each inventory by its UUID.
+//!
+//! ## Macros
+//! - [`report_change_on_inventory!($id)`]: Updates the last change timestamp for the given inventory UUID.
+//! - [`get_last_inventory_change!($id)`]: Retrieves the last change timestamp for the given inventory UUID, or `0` if not set.
+
+
+/// Global, thread-safe map storing the last change timestamp for each inventory by UUID.
 lazy_static! {
     pub static ref GLOBAL_MAP: Mutex<HashMap<String, u128>> = Mutex::new(HashMap::new());
 }
 
+/// Macro to report a change on an inventory by updating its last change timestamp to the current time (in milliseconds since UNIX epoch).
 #[macro_export]
 macro_rules! report_change_on_inventory {
     ($id:expr) => {
@@ -21,6 +33,9 @@ macro_rules! report_change_on_inventory {
     };
 }
 
+/// Macro to get the last change timestamp for an inventory by UUID.
+///
+/// Returns `0` if no timestamp is set for the given inventory.
 #[macro_export]
 macro_rules! get_last_inventory_change {
     ($id:expr) => {
--- backend/org_backend/src/locked_macros.rs
+++ backend/org_backend/src/locked_macros.rs
@@ -2,10 +2,22 @@ use std::sync::Mutex;
 
 use lazy_static::lazy_static;
 
+//! # Locked Macros
+//!
+//! This module provides global state and macros for managing a global "locked" status,
+//! which can be toggled or queried at runtime. The lock is stored as a thread-safe global boolean.
+//!
+//! ## Macros
+//! - [`lock_toggle!()`]: Toggles the global locked status.
+//! - [`locked_status!()`]: Returns the current locked status as a boolean.
+
+
+/// Global, thread-safe boolean indicating the locked status.
 lazy_static! {
     pub static ref GLOBAL_LOCKED: Mutex<bool> = Mutex::new(true);
 }
 
+/// Macro to toggle the global locked status.
 #[macro_export]
 macro_rules! lock_toggle {
     () => {
@@ -15,6 +27,7 @@ macro_rules! lock_toggle {
     };
 }
 
+/// Macro to get the current global locked status.
 #[macro_export]
 macro_rules! locked_status {
     () => {
