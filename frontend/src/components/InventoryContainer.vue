3335593aa5d29075f74a3031888d952eae8bdd34
Alex | Kronox <alexander_vogt@outlook.de>
2026-02-13 09:03:28 +0100

    fix function used in skip prop

--- frontend/src/components/InventoryContainer.vue
+++ frontend/src/components/InventoryContainer.vue
@@ -36,7 +36,7 @@
       <TimedConfirmationButton
         v-if="inventory.ownerUuid === store().uuid"
         class="ml-2 h-7 shrink-0 rounded-sm border border-amber-300 bg-fuchsia-900 px-1.5 text-red-300"
-        :skip-confirmation="() => Settings.getInstance().noDeleteConfirmation"
+        :skip-confirmation="Settings.getInstance().noDeleteConfirmation"
         @confirm="deleteInventory"
       >
         <FontAwesomeIcon :icon="faTrashCan" />
